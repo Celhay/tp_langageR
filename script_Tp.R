3f2430887608bd851fdc30b8ffe33a639bfac3da
Celhay <>
2021-03-10 10:37:42 +0100

    Exportation csv

--- script_Tp.R
+++ script_Tp.R
@@ -7,17 +7,18 @@ names(data) <- c("Canal","Region","ProduitsFrais","Lait","Epicerie","Surgele","D
 	#Suppression de la colonne Canal
 data$Canal <- NULL
 
-	#Ajout colonne Total de tout le tableau 
-#data$Total <- sum(as.numeric(data$ProduitsFrais,data$Lait,data$Epicerie,data$Surgele,data$Detergents,data$Traiteur))
 	#Ajout colonne Total de chaque colonne sauf Région
-data$TotalColonne <- rowSums(data[2:7])
+data$Total <- rowSums(data[2:7])
 
 	#Ajout colonne pourcentage de chiffre d’affaires des produits suivants arrondi à l'entier prés : 
-data$PourcentageProduitsFrais <- round(data$ProduitsFrais * 100 / data$TotalColonne)
-data$PourcentageLait <- round(data$Lait * 100 / data$TotalColonne)
-data$PourcentageEpicerie <- round(data$Epicerie * 100 / data$TotalColonne)
-data$PourcentageSurgele <- round(data$Surgele * 100 / data$TotalColonne)
-data$PourcentageDetergents <- round(data$Detergents * 100 / data$TotalColonne)
-data$PourcentageTraiteur <- round(data$Traiteur * 100 / data$TotalColonne)
+data$"% ProduitsFrais" <- round(data$ProduitsFrais * 100 / data$TotalColonne)
+data$"% Lait" <- round(data$Lait * 100 / data$TotalColonne)
+data$"% Epicerie" <- round(data$Epicerie * 100 / data$TotalColonne)
+data$"% Surgele" <- round(data$Surgele * 100 / data$TotalColonne)
+data$"% Detergents" <- round(data$Detergents * 100 / data$TotalColonne)
+data$"% Traiteur" <- round(data$Traiteur * 100 / data$TotalColonne)
+
+	#Exportation du tableau
+write.csv2(data, file = "D:/Epsi/EPSI I5/language R/tp/TPR.csv")
 
 str(data)
