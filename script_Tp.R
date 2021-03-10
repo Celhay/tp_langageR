db11e7c31ccbf87dc860be1eb1a94135716d24dc
Celhay <>
2021-03-10 10:27:40 +0100

    Calcul des pourcentages arrondi à l'entier prés

--- script_Tp.R
+++ script_Tp.R
@@ -12,12 +12,12 @@ data$Canal <- NULL
 	#Ajout colonne Total de chaque colonne sauf Région
 data$TotalColonne <- rowSums(data[2:7])
 
-	#Ajout colonne pourcentage de chiffre d’affaires des produits suivants : 
-data$PourcentageProduitsFrais <- data$ProduitsFrais * 100 / data$TotalColonne
-data$PourcentageLait <- data$Lait * 100 / data$TotalColonne
-data$PourcentageEpicerie <- data$Epicerie * 100 / data$TotalColonne
-data$PourcentageSurgele <- data$Surgele * 100 / data$TotalColonne
-data$PourcentageDetergents <- data$Detergents * 100 / data$TotalColonne
-data$PourcentageTraiteur <- data$Traiteur * 100 / data$TotalColonne
+	#Ajout colonne pourcentage de chiffre d’affaires des produits suivants arrondi à l'entier prés : 
+data$PourcentageProduitsFrais <- round(data$ProduitsFrais * 100 / data$TotalColonne)
+data$PourcentageLait <- round(data$Lait * 100 / data$TotalColonne)
+data$PourcentageEpicerie <- round(data$Epicerie * 100 / data$TotalColonne)
+data$PourcentageSurgele <- round(data$Surgele * 100 / data$TotalColonne)
+data$PourcentageDetergents <- round(data$Detergents * 100 / data$TotalColonne)
+data$PourcentageTraiteur <- round(data$Traiteur * 100 / data$TotalColonne)
 
 str(data)
