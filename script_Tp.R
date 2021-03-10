bb690bb15ecbb927a906df0b8e2303ca02160f50
Celhay <>
2021-03-10 10:24:15 +0100

    Calcul des pourcentages

--- script_Tp.R
+++ script_Tp.R
@@ -12,10 +12,12 @@ data$Canal <- NULL
 	#Ajout colonne Total de chaque colonne sauf Région
 data$TotalColonne <- rowSums(data[2:7])
 
-
-data$%ProduitsFrais <- rowSums(data[2:7])
-
-
-
+	#Ajout colonne pourcentage de chiffre d’affaires des produits suivants : 
+data$PourcentageProduitsFrais <- data$ProduitsFrais * 100 / data$TotalColonne
+data$PourcentageLait <- data$Lait * 100 / data$TotalColonne
+data$PourcentageEpicerie <- data$Epicerie * 100 / data$TotalColonne
+data$PourcentageSurgele <- data$Surgele * 100 / data$TotalColonne
+data$PourcentageDetergents <- data$Detergents * 100 / data$TotalColonne
+data$PourcentageTraiteur <- data$Traiteur * 100 / data$TotalColonne
 
 str(data)
