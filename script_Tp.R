178f3738aef35648ba29e3e6e88f77bf1e03bc9d
Celhay <>
2021-03-10 12:22:13 +0100

    moyenne partie 1

--- script_Tp.R
+++ script_Tp.R
@@ -20,22 +20,20 @@ data$"% Traiteur" <- round(data$Traiteur * 100 / data$TotalColonne)
 	#Exportation du tableau
 write.csv2(data, file = "D:/Epsi/EPSI I5/language R/tp/TPR.csv")
 
-str(data)
+	#Chiffre d’affaire « Total » moyen
+apply(data[8],2,tapply,data$Region,mean)
 
 
-
-#Partie 2
+#Partie 2 :
 
 data <- read.csv("C:/Users/rems_/OneDrive/Bureau/R/tp_langageR/ventes.csv")
 names(data) <- c("Canal","Region","ProduitsFrais","Lait","Epicerie","Surgele","Detergents","Traiteur")
 
-#Question 1 : Determine quel paire de vaiable a le meilleur coéfficient de coréaltion linéaire
-
+	#Meilleur coéfficient de coréaltion linéaire
 cor(data[-2][-1], use="complete.obs")
-
 #On peut voir que c'est la paire Détergent - Epicerie
 
-#Question 2
+	#Question 2
 plot(data$Detergents, data$Epicerie, xlab = "Detergents", ylab = "Epicerie")
 reg <- lm(Epicerie ~ Detergents, data = data)
 abline(reg)
@@ -44,7 +42,7 @@ plot(data$Epicerie , data$Detergents, xlab = "Epicerie", ylab = "Détergents")
 reg2 <- lm(Detergents ~ Epicerie, data = data)
 abline(reg2)
 
-#Question 3
+	#Question 3
 
 prev <- predict(reg, data.frame(Traiteur = Detergents*2))
 prev
